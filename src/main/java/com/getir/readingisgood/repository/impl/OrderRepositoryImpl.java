fc3940045c80899973112e61fabf96c284da2675
Burak Kabasakal <kabasakalburak@hotmail.com>
2021-09-17 13:34:21 +0300

    statistics bugfix

--- src/main/java/com/getir/readingisgood/repository/impl/OrderRepositoryImpl.java
+++ src/main/java/com/getir/readingisgood/repository/impl/OrderRepositoryImpl.java
@@ -22,9 +22,16 @@ public class OrderRepositoryImpl implements OrderRepositoryCustom {
     @Autowired
     MongoTemplate mongoTemplate;
 
-    @Override
+       @Override
     public List<StatisticDto> monthlyOrderStatisticsForCustomer(String customerId, OrderState orderState) {
-
+        //find customer with APPROVED state
+        //order's amount and count specified fields group with createdDate months
+        //then prepare totalOrderCount, totalBookCount and totalAmount
+        // and sort with createdDate
+        /**
+         *  NOTE: Some of decimal amount values like 3.5234234, 5.5345435 sometimes give the wrong total because of scale.
+         *  Each of them should firstly scale and added.
+          */
         final Aggregation aggregation = newAggregation(
                 match(Criteria.where("orderState").is(orderState).and("customerId").is(customerId)),
 
@@ -32,11 +39,12 @@ public class OrderRepositoryImpl implements OrderRepositoryCustom {
                 group("month").count().as("totalOrderCount")
                         .sum("count").as("totalBookCount")
                 .sum("amount").as("totalAmount"),
-                project("totalOrderCount","totalBookCount","totalAmount").and("month").previousOperation(),sort(Sort.Direction.ASC,"month")
+                project("totalOrderCount","totalBookCount","totalAmount").and("month")
+                        .previousOperation(), //
+                sort(Sort.Direction.ASC,"month")
         );
 
-            return mongoTemplate.aggregate(aggregation, Order.class,StatisticDto.class).getMappedResults();
-
+            return mongoTemplate.aggregate(aggregation,Order.class,StatisticDto.class).getMappedResults();
 // https://www.mongodb.com/community/forums/t/group-query-result-by-month-in-mongotemplate/117680
 // https://gist.github.com/japharr/f15a871d853ff45398f7f02667000e51
 
